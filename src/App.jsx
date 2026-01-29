c90bdc60a28ccc0a801cfa925e9e630bb1516b10
Kunjbihari23 <kunjbihari.h@devstree.in>
2026-01-29 11:46:16 +0530

    feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import RubiksCube from "./components/RubiksCube";
+import ThreeBasic from "./components/ThreeBasics";
+
+const App = () => {
+  //  return <ThreeBasic />;
+  return <RubiksCube />;
+};
+
+export default App;
