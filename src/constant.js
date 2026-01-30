162994ec1013c8592427edca8c23632b2e64e0fa
kunjbihari <kunjbihari.h@devstree.in>
2026-01-30 13:05:32 +0530

    feat: added image in constants for easy access

---
+++ src/constant.js
@@ -0,0 +1,50 @@
+import gradiant1 from "../static/textures/gradients/3.jpg";
+import gradiant2 from "../static/textures/gradients/5.jpg";
+
+import matcap1 from "../static/textures/matcaps/1.png";
+import matcap2 from "../static/textures/matcaps/2.png";
+import matcap3 from "../static/textures/matcaps/3.png";
+import matcap4 from "../static/textures/matcaps/4.png";
+import matcap5 from "../static/textures/matcaps/5.png";
+import matcap6 from "../static/textures/matcaps/6.png";
+import matcap7 from "../static/textures/matcaps/7.png";
+import matcap8 from "../static/textures/matcaps/8.png";
+
+import alpha from "../static/textures/door/alpha.jpg";
+import ambientOcclusion from "../static/textures/door/ambientOcclusion.jpg";
+import color from "../static/textures/door/color.jpg";
+import height from "../static/textures/door/height.jpg";
+import metalness from "../static/textures/door/metalness.jpg";
+import normal from "../static/textures/door/normal.jpg";
+import roughness from "../static/textures/door/roughness.jpg";
+
+import environmentMap from "../static/textures/environmentMap/2k.hdr";
+
+export const IMAGES = {
+  gradiants: {
+    gradiant1,
+    gradiant2,
+  },
+  matcaps: {
+    matcap1,
+    matcap2,
+    matcap3,
+    matcap4,
+    matcap5,
+    matcap6,
+    matcap7,
+    matcap8,
+  },
+  environmentMap: {
+    environmentMap,
+  },
+  door: {
+    alpha,
+    ambientOcclusion,
+    color,
+    height,
+    metalness,
+    normal,
+    roughness,
+  },
+};
