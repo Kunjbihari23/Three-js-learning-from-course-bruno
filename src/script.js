d57d6a8031870e85b4f549338d962e07f718c30c
kunjbihari <kunjbihari.h@devstree.in>
2026-02-11 12:01:43 +0530

    feat: completed galaxy theme using particles

--- src/script.js
+++ src/script.js
@@ -25,12 +25,14 @@ const particalTexture = textureLoader.load("./textures/particles/2.png");
  */
 
 const ParticlesGeometry = new THREE.BufferGeometry();
-const count = 400;
+const count = 600;
 
 const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
 
 for (let index = 0; index < count * 3; index++) {
   positions[index] = (Math.random() - 0.5) * 10;
+  colors[index] = Math.random();
 }
 
 ParticlesGeometry.setAttribute(
@@ -38,13 +40,16 @@ ParticlesGeometry.setAttribute(
   new THREE.BufferAttribute(positions, 3),
 );
 
+ParticlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+
 // const ParticlesGeometry = new THREE.SphereGeometry(1, 32, 32);
 const ParticlesMaterial = new THREE.PointsMaterial();
 
-ParticlesMaterial.color = new THREE.Color(0x00ff00);
-ParticlesMaterial.size = 0.08;
-ParticlesMaterial.sizeAttenuation = true;
+// ParticlesMaterial.color = new THREE.Color(0x00ff00);
+ParticlesMaterial.size = 0.1;
+ParticlesMaterial.sizeAttenuation = true; // Particles look smaller when further away
 ParticlesMaterial.alphaMap = particalTexture;
+ParticlesMaterial.vertexColors = true; // Tells material to use the random 'colors' array we created above
 
 /**
  * this three can fix the proper rendering of alpha texture but best way is
@@ -52,8 +57,9 @@ ParticlesMaterial.alphaMap = particalTexture;
  * depthTest false work but issue is is generate unexpected results for other objs */
 // ParticlesMaterial.alphaTest = 0.001; // this will work but still got issue in some
 // ParticlesMaterial.depthTest = false;
-ParticlesMaterial.depthWrite = false;
+ParticlesMaterial.depthWrite = false; // Solving Transparency Issues
 ParticlesMaterial.transparent = true;
+ParticlesMaterial.blending = THREE.AdditiveBlending; // When particles overlap, their colors are added together. This makes dense clusters of particles look glowing and bright (like fire or magic effects).
 
 /**
  * Points
@@ -115,6 +121,16 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  for (let index = 0; index < count; index++) {
+    const i3 = index * 3;
+    const x = ParticlesGeometry.attributes.position.array[i3];
+    ParticlesGeometry.attributes.position.array[i3 + 1] = Math.cos(
+      elapsedTime + x,
+    );
+  }
+
+  ParticlesGeometry.attributes.position.needsUpdate = true;
 
   // Update controls
   controls.update();
